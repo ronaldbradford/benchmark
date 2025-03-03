41064826026ed8b97601c9ae38077b240f80cb9d
Ronald Bradford <ronald.bradford@gmail.com>
2025-03-03 11:03:34 -0500

    Example SQL Corrections

--- imdb/title.sql
+++ imdb/title.sql
@@ -1,7 +1,6 @@
 SELECT * FROM title t WHERE t.title = ? AND t.type = 'movie';
-SELECT t.title, r.average_rating, r.num_votes FROM title t JOIN title_rating r USING (tconst) WHERE t.title = ? AND t.type = 'movie';
-SELECT DISTINCT n.name, cn.name FROM name n JOIN title_character tc USING (name_id) JOIN title t USING (title_id) JOIN character_name cn USING (character_id) WHERE t.title = ?  AND t.type = 'movie';
+SELECT t.title, r.average_rating, r.num_votes FROM title t JOIN title_rating r USING (title_id) WHERE t.title = ? AND t.type = 'movie';
 SELECT g.genre FROM title_genre g JOIN title t USING (title_id) WHERE t.title = ? AND t.type = 'movie';
-SELECT DISTINCT n.name AS name, cn.name AS character_name FROM name n JOIN title_character tc USING (name_id) JOIN title t USING (title_id) JOIN character_name cn USING (character_id) WHERE t.title = ? AND t.type = 'movie';
+SELECT DISTINCT n.name AS name, tnc.character_name FROM name n JOIN title_name_character tnc USING (name_id) JOIN title t USING (title_id) WHERE t.title = ? AND t.type = 'movie';
 
 
