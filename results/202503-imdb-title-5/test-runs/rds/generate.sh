f6cc61a24f8b824f39e200cce0eacf924f7a038f
Ronald Bradford <ronald.bradford@gmail.com>
2025-03-31 20:10:06 -0400

    Added 8xl to r6g results

--- results/202503-imdb-title-5/test-runs/rds/generate.sh
+++ results/202503-imdb-title-5/test-runs/rds/generate.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
+RESULTS_FILE=${RESULTS_FILE:-results.csv}
+(
 # We need to control the order of instance sizes.
-(echo "Threads,Test,Transactions"; for dir in readyset-r6i.large readyset-r6i.xlarge readyset-r6i.2xlarge; do     awk -F, -v dir="$dir" '{printf("%s,%s,%s\n", $1, dir, $4)}' $dir/*.csv | grep -v "^Threads" | sort -n; done) | tee rds-results.csv
+  ORDERED_DIRS="rds-mysql-r6g.2xlarge rds-mysql-r6g.4xlarge rds-mysql-r6g.8xlarge"
+  echo "Threads,Test,Transactions"
+  for dir in ${ORDERED_DIRS}; do     
+    awk -F, -v dir="$dir" '{printf("%s,%s,%s\n", $1, dir, $4)}' $dir/*.csv | grep -v "^Threads" | sort -n;
+  done 
+) | tee "${RESULTS_FILE}"
